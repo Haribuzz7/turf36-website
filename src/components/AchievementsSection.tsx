d97ca55fad07104235684957905f15ab13f98019
haribuzz <harrypictureshp@gmail.com>
2026-07-14 19:29:59 +0530

    Fix use client directives stripped by regex

--- src/components/AchievementsSection.tsx
+++ src/components/AchievementsSection.tsx
@@ -1,3 +1,4 @@
+"use client";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/no-unescaped-entities */
 
--- src/components/EventsSection.tsx
+++ src/components/EventsSection.tsx
@@ -1,3 +1,4 @@
+"use client";
 
 import SectionHighlight from "./SectionHighlight";
 
